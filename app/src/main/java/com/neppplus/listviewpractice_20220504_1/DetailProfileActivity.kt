198bda3e077bb3325bd71bd7790476839b63c9f1
nepp.plus <nepp.plus@gamil.com>
2022-05-04 14:37:03 +0900

    추가 문제 1 . 프로필 화면 꾸미기

---
+++ app/src/main/java/com/neppplus/listviewpractice_20220504_1/DetailProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.neppplus.listviewpractice_20220504_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_profile)
+
+    }
+}
