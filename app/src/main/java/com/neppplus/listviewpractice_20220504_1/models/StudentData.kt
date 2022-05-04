9d450b9df2c6647a243aa078adec905b8e009e23
nepp.plus <nepp.plus@gamil.com>
2022-05-04 10:24:58 +0900

    3. StudentData 클래스 파일의 멤버 변수 생성

--- app/src/main/java/com/neppplus/listviewpractice_20220504_1/models/StudentData.kt
+++ app/src/main/java/com/neppplus/listviewpractice_20220504_1/models/StudentData.kt
@@ -1,4 +1,8 @@
 package com.neppplus.listviewpractice_20220504_1.models
 
-class StudentData {
+class StudentData (
+    val name : String,
+    val birthYear : Int,
+    val address : String,
+        ) {
 }
